7fa92c111df33c1b8a5c942ebc3ad1b9d7ccb3c9
Hemin Johnson <hemin.johnson@diconium.com>
2019-09-12 16:20:53 +0200

    refactor: use apollo client to handle the collection component

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from 'react'
 
-import CollectionItem from '../../components/collection-item/collection-item.component';
+import CollectionItem from '../../components/collection-item/collection-item.component'
 
-import { selectCollection } from '../../redux/shop/shop.selectors';
-
-import './collection.styles.scss';
+import './collection.styles.scss'
 
 const CollectionPage = ({ collection }) => {
-  const { title, items } = collection;
+  const { title, items } = collection
   return (
     <div className='collection-page'>
       <h2 className='title'>{title}</h2>
@@ -18,11 +15,7 @@ const CollectionPage = ({ collection }) => {
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.container'
-import CollectionPage from '../collection/collection.component'
+import CollectionPage from '../collection/collection.container'
 
 const ShopPage = ({ match }) => (
   <div className='shop-page'>
